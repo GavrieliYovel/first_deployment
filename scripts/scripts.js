742a0ad76fd8e560c661756d5c72290fb8c5b11c
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2022-04-23 19:37:57 +0300

    query + form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,17 @@ function func(i, val)  {
     outputs[i].value = val;
 };
 
+
+$(document).ready(function() {
+    $("#standStill").click(function()
+    {
+        console.log($(this).val());
+            $("#maxDistance").attr("disabled", "disabled");
+    });
+
+    $("#patrol").click(function()
+    {
+        console.log($(this).val());
+            $("#maxDistance").removeAttr("disabled");
+    });
+});
